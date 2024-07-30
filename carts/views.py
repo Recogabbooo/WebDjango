eb11af1615d14b73d4d8c42991b09f79d5da57dc
Recogabbooo <137157203+Recogabbooo@users.noreply.github.com>
2024-07-30 11:49:24 +0200

    avanzandoII

--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,8 @@ from .models import Cart
 from products.models import Product
 from .funciones import funcionCarrito
 from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
+
 
 def cart(request):
     cart = funcionCarrito(request)
@@ -14,7 +16,7 @@ def cart(request):
 
 def add(request):
     cart = funcionCarrito(request)
-    product = Product.objects.get(pk=request.POST.get('product_id'))
+    product = get_object_or_404 (Product, pk=request.POST.get('product_id'))
     
     cart.products.add(product)
     
@@ -24,7 +26,7 @@ def add(request):
     
 def remove(request):
         cart = funcionCarrito(request)
-        product = Product.objects.get(pk=request.POST.get('product_id'))
+        product = get_object_or_404 (Product, pk=request.POST.get('product_id'))
         
         cart.products.remove(product)
 
